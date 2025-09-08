98a20c5d482b74e79c7a34bf2cb687c4321fa4ab
felipejf95 <felipejf95@hotmail.com>
2025-09-08 20:57:13 -0300

    Criação do projeto/Spring Initializr

---
+++ desafio-todo/src/main/java/br/com/felipe/desafio_todo/DesafioTodoApplication.java
@@ -0,0 +1,13 @@
+package br.com.felipe.desafio_todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioTodoApplication.class, args);
+	}
+
+}
---
+++ desafio-todo/src/test/java/br/com/felipe/desafio_todo/DesafioTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.felipe.desafio_todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
